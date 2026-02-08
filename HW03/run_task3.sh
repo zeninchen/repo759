3efcb49be8fefeafc39441b5ae316fcc4d892fa6
unknown <zenin.chen2015@gmail.com>
2026-02-08 15:53:39 -0600

    d

---
+++ HW03/run_task3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+#SBATCH -p instruction
+#SBATCH --job-name=task3
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
+#SBATCH --time=00:01:00
+#SBATCH --output=task3.out
+#SBATCH --error=task3.err
+cd $SLURM_SUBMIT_DIR
+
+./task3 
