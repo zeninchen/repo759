adea84c3ba788127890be5505dd833f1c0bce31b
unknown <zenin.chen2015@gmail.com>
2026-02-09 14:33:28 -0600

    d

---
+++ HW03/run_task3_scale.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+#SBATCH -p instruction
+#SBATCH --job-name=task3
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
+#SBATCH --time=00:01:00
+#SBATCH --output=task3.out
+#SBATCH --error=task3.err
+cd $SLURM_SUBMIT_DIR
+
+./task3_scale 
+./task3_scale 16 task3_16.csv
