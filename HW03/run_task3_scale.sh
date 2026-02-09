488faf68002410268484edde7a125ec8513f34d2
unknown <zenin.chen2015@gmail.com>
2026-02-09 14:39:43 -0600

    d

--- HW03/run_task3_scale.sh
+++ HW03/run_task3_scale.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=task3
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-task=1
+#SBATCH --gpus-per-task=5
 #SBATCH --time=00:10:00
 #SBATCH --output=task3.out
 #SBATCH --error=task3.err
