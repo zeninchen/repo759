bb9851420415b255d8462c4cbffc2ed6ddea4770
unknown <zenin.chen2015@gmail.com>
2026-02-12 17:23:34 -0600

    d

---
+++ HW04/run_task1.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+#SBATCH -p instruction
+#SBATCH --job-name=task1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=5
+#SBATCH --time=00:10:00
+#SBATCH --output=task1.out
+#SBATCH --error=task1.err
+cd $SLURM_SUBMIT_DIR
+
+./task1
