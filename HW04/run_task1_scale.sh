223a1c164fa75f933103fce46dae2edfc39370c6
unknown <zenin.chen2015@gmail.com>
2026-02-12 17:45:41 -0600

    d

---
+++ HW04/run_task1_scale.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+#SBATCH -p instruction
+#SBATCH --job-name=task1_scale
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=5
+#SBATCH --time=00:10:00
+#SBATCH --output=task1_scale.out
+#SBATCH --error=task1_scale.err
+cd $SLURM_SUBMIT_DIR
+./task1_scale
