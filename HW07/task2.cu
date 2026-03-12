e58637830438ac5028bafd7dce2bed20e6e10d27
unknown <zenin.chen2015@gmail.com>
2026-03-12 18:09:16 -0500

    d

--- HW07/task2.cu
+++ HW07/task2.cu
@@ -1,15 +1,18 @@
+#include <cstdio>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 #include "count.cuh"
 
 int main(int argc, char* argv[])
 {
-    // Example usage
-    thrust::device_vector<int> d_in = {3, 5, 1, 2, 3, 1};
+    thrust::host_vector<int> h_in = {3, 5, 1, 2, 3, 1};
+    thrust::device_vector<int> d_in = h_in;
+
     thrust::device_vector<int> values;
     thrust::device_vector<int> counts;
 
     count(d_in, values, counts);
 
-    // Print results
     for (size_t i = 0; i < values.size(); ++i) {
         printf("Value: %d, Count: %d\n", values[i], counts[i]);
     }
